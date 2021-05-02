bb6138e56be56e0f69b0c165c9d2297b0017b4d8
jackhuan <huanxingan1314@163.com>
2021-05-02 10:06:19 +0800

    6. 判断字符是否唯一

--- algorithm/strIsUnique/index.ts
+++ algorithm/strIsUnique/index.ts
@@ -1,10 +1,15 @@
 /**
  * @author Jack huan 最帅的坏兔子
- * @date 2021-04-27
+ * @date 2021-05-02
  * @description 判定字符是否唯一
  * @param {string} astr 目标字符串
  * @return {boolean} 函数返回值
  */
 export const isUnique = (astr: string): boolean => {
-    return false
+    for(let i:number = 0; i < astr.length; i++) {
+        for(let j:number = i + 1; j < astr.length; j++) {
+            if(astr.charAt(i) === astr.charAt(j)) return false;
+        }
+    }
+    return true;
 }
