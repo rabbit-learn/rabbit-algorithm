7a5c70226c8252e360459ae9703aee607ee22581
jackhuan <huanxingan1314@163.com>
2021-05-17 23:51:40 +0800

    实现strStr

--- algorithm/implementStrstr/index.ts
+++ algorithm/implementStrstr/index.ts
@@ -7,5 +7,41 @@
  * @return {number} 函数返回值
  */
 export const strStr = (haystack: string, needle: string ): number => {
-    
+    const needleLength: number = needle.length;
+    if(needleLength === 0) return 0;
+    let index: number = -1;
+    let isJudge: boolean = false;
+    let currentIndex: number = 0;
+    let isEqual: boolean = false;
+    for (let i = 0; i < haystack.length; i++) {
+        const c = haystack.charAt(i);
+        if(isJudge){
+            if(c === needle.charAt(currentIndex)) {
+                if(currentIndex === needleLength - 1){
+                    isEqual = true;
+                    break;
+                } else {
+                    ++currentIndex;
+                }
+            } else {
+                i = index;
+                index = -1;
+                isJudge = false;
+                currentIndex = 0;
+            }
+        } else {
+            if(c === needle.charAt(0)) {
+                index = i;
+                if(currentIndex === needleLength - 1) {
+                    isEqual = true;
+                    break;
+                } else {
+                    isJudge = true;
+                    ++currentIndex;
+                }
+            }
+        }
+        
+    }
+    return isEqual ? index : -1;
 }
