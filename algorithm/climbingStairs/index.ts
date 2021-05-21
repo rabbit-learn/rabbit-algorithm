3380f83ce5a0199bfb94b0c045e1d5d22521dcb4
Jack huan <huanxingan1314@163.com>
2021-05-21 22:05:37 +0800

    debug

--- algorithm/climbingStairs/index.ts
+++ algorithm/climbingStairs/index.ts
@@ -8,7 +8,8 @@
 export const climbStairs = (n: number ): number => {
     let currentTotal = 1, lastTotal = 1, m = 1;
     while (m < n) {
-        const temp = currentTotal;
+        let temp = 0;
+        temp += currentTotal;
         lastTotal = temp;
         ++m;
     }
