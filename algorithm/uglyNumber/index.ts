711cfe1b649bcdd9c0bcff320acd8d67fde6b01c
Jack huan <huanxingan1314@163.com>
2021-05-27 07:45:24 +0800

    丑数

--- algorithm/uglyNumber/index.ts
+++ algorithm/uglyNumber/index.ts
@@ -1,22 +1,24 @@
 /**
  * @author Jack huan 最帅的坏兔子
- * @date 2021-05-22
- * @description 计数质数
- * @param {number} n 目标值
- * @return {number} 函数返回值
+ * @date 2021-05-23
+ * @description 丑数
+ * @param {number} num 目标值
+ * @return {boolean} 函数返回值
  */
-export const countPrimes = (n: number ): number => {
-    if(n < 3) return 0;
-    let num = 1;
-    for (let i = 3; i < n; i+2) {
-        let isPrime = true;
-        for (let j = 3; j**2 < i; j+2) {
-            if(i * j === 0) {
-                isPrime = false;
-                break;
-            }
-        }
-        if(isPrime) ++num;
+export const isUgly = (num: number ): boolean => {
+    if(num < 1){
+        return false
     }
-    return num;
+}
+
+const divide = (n: number) => {
+    if(n % 2 === 0) {
+        return Math.floor(n / 2)
+    }else if(n % 3 === 0){
+        return Math.floor(n / 3)
+    }else if(n % 5 === 0){
+        return Math.floor(n / 5)
+    }else{
+        return -1
+    }
 }
