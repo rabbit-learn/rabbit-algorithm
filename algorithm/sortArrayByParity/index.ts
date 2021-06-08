9f3b89618b014cfacafc4a2309b6c5248d0ca690
Jack huan <huanxingan1314@163.com>
2021-06-08 22:40:01 +0800

    按奇偶排序数组

--- algorithm/sortArrayByParity/index.ts
+++ algorithm/sortArrayByParity/index.ts
@@ -1,15 +1,22 @@
 /**
  * @author Jack huan 最帅的坏兔子
- * @date 2021-05-24
- * @description 存在重复元素
- * @param {number[]} nums 目标值
- * @return {boolean} 函数返回值
+ * @date 2021-05-26
+ * @description 按奇偶排序数组
+ * @param {number[]} A 目标值
+ * @return {number[]} 函数返回值
  */
-export const containsDuplicate = (nums: number[]): boolean => {
-  const set = new Set()
-  for(const num of nums) {
-    if(set.has(num)) return true;
-    set.add(num)
+export const sortArrayByParity = (A: number[]): number[] => {
+  const B: number[] = []
+  let eventIndex: number = 0;
+  let oddIndex: number = 0;
+  for(let value of A) {
+    if(value % 2 === 0){
+      B[eventIndex] = value;
+      ++eventIndex;
+    }else{
+      B[oddIndex] = value;
+      --oddIndex;
+    }
   }
-  return false
+  return B;
 };
--- test/sortArrayByParity.spec.ts
+++ test/sortArrayByParity.spec.ts
@@ -1,9 +1,9 @@
-import { containsDuplicate } from "../algorithm/containsDuplicate";
+import { sortArrayByParity } from "../algorithm/sortArrayByParity";
 
-describe("存在重复元素", () => {
-  test("containsDuplicate", () => {
-    expect(containsDuplicate([1,2,3,1])).toEqual(true);
-    expect(containsDuplicate([1,1,1,3,3,4,3,2,4,2])).toEqual(true);
-    expect(containsDuplicate([1,2,3,4])).toEqual(false);
+describe("按奇偶排序数组", () => {
+  test("sortArrayByParity", () => {
+    expect(sortArrayByParity([1,2,3,1])).toEqual(true);
+    expect(sortArrayByParity([1,1,1,3,3,4,3,2,4,2])).toEqual(true);
+    expect(sortArrayByParity([1,2,3,4])).toEqual(false);
   });
 });
